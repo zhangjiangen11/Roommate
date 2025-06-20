56bdf32f70b1da9046793604bf7b326693abb802
Hoork <hoorkgames@yandex.ru>
2025-06-20 17:45:39 +0500

    added rotation to bevel area

--- addons/roommate/nodes/bevel.gd
+++ addons/roommate/nodes/bevel.gd
@@ -11,24 +11,65 @@ class_name RoommateBevel
 extends RoommateBlocksArea
 ## Area that represents sloped surface
 
+enum ExtendAxis {
+	X = 0,
+	Y = 1,
+	Z = 2,
+}
+enum BevelRotation {
+	A,
+	B,
+	C,
+	D,
+}
+
+@export var extend_axis := ExtendAxis.X
+@export var bevel_rotation := BevelRotation.A
+
+var _endpoints := {
+	ExtendAxis.X: {
+		BevelRotation.A: [4, 0],
+		BevelRotation.B: [6, 2],
+		BevelRotation.C: [7, 3],
+		BevelRotation.D: [5, 1],
+	},
+	ExtendAxis.Y: {
+		BevelRotation.A: [6, 4],
+		BevelRotation.B: [2, 0],
+		BevelRotation.C: [3, 1],
+		BevelRotation.D: [7, 5],
+	},
+	ExtendAxis.Z: {
+		BevelRotation.A: [5, 4],
+		BevelRotation.B: [7, 6],
+		BevelRotation.C: [3, 2],
+		BevelRotation.D: [1, 0],
+	},
+}
+
 
 func _process_block(new_block: RoommateBlock, blocks_range: AABB) -> RoommateBlock:
 	const HALF := Vector3.ONE / 2
 	
-	var ignored_axis_index := 0
+	var scale_axis := Vector3.UP
+	if extend_axis == ExtendAxis.Z:
+		scale_axis = Vector3.RIGHT
+	
 	var used_size := blocks_range.size
-	used_size[ignored_axis_index] = 0
+	used_size[extend_axis] = 0
 	
 	var max_side_size := used_size[used_size.max_axis_index()]
 	
 	var next_direction := Vector3.ONE
-	next_direction[ignored_axis_index] = 0
+	next_direction[extend_axis] = 0
 	next_direction[used_size.max_axis_index()] = 0
 	
 	var next := Vector3.ZERO
-	next[ignored_axis_index] = 1
-	var plane := Plane(blocks_range.position - HALF, blocks_range.end - HALF, 
-			blocks_range.position + next - HALF)
+	next[extend_axis] = 1
+	
+	var endpoints := _endpoints[extend_axis][bevel_rotation] as Array
+	var plane := Plane(blocks_range.get_endpoint(endpoints[0]) - HALF, 
+			blocks_range.get_center() - HALF, blocks_range.get_endpoint(endpoints[1]) - HALF)
 	
 	var is_on_plane := plane.has_point(new_block.position, 0.5)
 	var next_is_on_plane := plane.has_point((new_block.position as Vector3) + next_direction, 0.5)
@@ -41,10 +82,10 @@ func _process_block(new_block: RoommateBlock, blocks_range: AABB) -> RoommateBlo
 	var anchor := intersection - (new_block.position as Vector3) + HALF
 	
 	var a := Vector3.ONE
-	a[ignored_axis_index] = 0
+	a[extend_axis] = 0
 	var bevel_length := (blocks_range.size * a).length()
-	var part_scale := (bevel_length - max_side_size) / max_side_size + 1
-	var part_transform := Transform3D.IDENTITY.looking_at(plane.normal, Vector3.FORWARD).scaled_local(Vector3(1, part_scale, 1))
+	var part_scale_delta := (bevel_length - max_side_size) / max_side_size
+	var part_transform := Transform3D.IDENTITY.looking_at(plane.normal, Vector3.FORWARD).scaled_local(Vector3.ONE + part_scale_delta * scale_axis)
 	var bevel_part := _create_default_part(anchor, Vector3.ZERO, part_transform)
 	
 	new_block.type_id = RoommateBlock.BEVEL_TYPE;
