67c1402f50ec52a5936b773a8ee2d95ce92aa7a7
Hoork <hoorkgames@yandex.ru>
2025-07-28 04:27:56 +0500

    Update blocks_area.gd

--- addons/roommate/nodes/blocks_area.gd
+++ addons/roommate/nodes/blocks_area.gd
@@ -96,8 +96,8 @@ func snap_to_range(root_transform: Transform3D, block_size: float) -> void:
 	range.size *= block_size
 	range.position *= block_size
 	range = range.grow(-absf(SETTINGS.get_float(&"stid_area_snap_margin")))
-	global_rotation = root_transform.basis.get_euler() + rotation.snapped(Vector3.ONE * PI / 2)
-	global_position = root_transform * range.get_center()
+	var new_basis := Basis.from_euler(rotation.snapped(Vector3.ONE * PI / 2))
+	global_transform = root_transform * Transform3D(new_basis, range.get_center())
 	size = global_transform.affine_inverse().basis * (root_transform.basis * range.size)
 
 
