ed6cc6b9e8a37d611edf9ee502505df37dbfd464
Hoork <hoorkgames@yandex.ru>
2025-08-24 02:28:31 +0500

    undo redo for generate

--- addons/roommate/editor_actions.gd
+++ addons/roommate/editor_actions.gd
@@ -19,10 +19,87 @@ func _init(plugin: EditorPlugin) -> void:
 func generate_roots(roots: Array[RoommateRoot]) -> void:
 	if roots.is_empty():
 		return
+	
 	_ur.create_action("ROOMMATE: Generate Root(s)")
+	
 	for root in roots:
+		# pre-generate scenes
+		# removing scenes here so generate won't delete them
+		var cleared_scene_infos: Array[Dictionary] = []
+		var cleared_scenes := root.get_owned_scenes()
+		for scene in cleared_scenes:
+			_ur.add_do_method(scene.get_parent(), &"remove_child", scene)
+			cleared_scene_infos.append({
+				&"node": scene,
+				&"parent": scene.get_parent(),
+				&"owner": scene.owner,
+			})
+		for scene in cleared_scenes:
+			scene.get_parent().remove_child(scene)
+		
+		# pre-generate mesh
+		var old_mesh_container := root.get_node_or_null(root.linked_mesh_container) as Node3D
+		var old_mesh_instance := old_mesh_container as MeshInstance3D
+		if old_mesh_instance:
+			_ur.add_undo_property(old_mesh_instance, &"mesh", old_mesh_instance.mesh)
+		elif old_mesh_container:
+			pass
+		_ur.add_undo_property(root, &"linked_mesh_container", root.linked_mesh_container)
+		
+		# pre-generate collision
+		var collision_shape := root.get_node_or_null(root.linked_collision_shape) as CollisionShape3D
+		if collision_shape:
+			_ur.add_undo_property(collision_shape, &"shape", collision_shape.shape)
+		
+		# pre-generate nav
+		var nav_region := root.get_node_or_null(root.linked_navigation_region) as NavigationRegion3D
+		if nav_region:
+			_ur.add_undo_property(nav_region, &"navigation_mesh", nav_region.navigation_mesh)
+		
+		# generating everything...
 		root.generate()
-	_ur.commit_action()
+		if nav_region:
+			await nav_region.bake_finished
+		
+		# post-generate mesh
+		var new_mesh_container := root.get_node_or_null(root.linked_mesh_container) as Node3D
+		var new_mesh_instance := new_mesh_container as MeshInstance3D
+		
+		if not old_mesh_container and new_mesh_container:
+			_ur.add_do_method(new_mesh_container.get_parent(), &"add_child", new_mesh_container)
+			_ur.add_do_property(new_mesh_container, &"owner", new_mesh_container.owner)
+			_ur.add_do_reference(new_mesh_container)
+			_ur.add_undo_method(new_mesh_container.get_parent(), &"remove_child", new_mesh_container)
+		
+		if new_mesh_instance:
+			_ur.add_do_property(new_mesh_instance, &"mesh", new_mesh_instance.mesh)
+		elif new_mesh_container:
+			pass
+		_ur.add_do_property(root, &"linked_mesh_container", root.linked_mesh_container)
+		
+		# pre-generate collision
+		if collision_shape:
+			_ur.add_do_property(collision_shape, &"shape", collision_shape.shape)
+		
+		# pre-generate nav
+		if nav_region:
+			_ur.add_do_property(nav_region, &"navigation_mesh", nav_region.navigation_mesh)
+		
+		# post-generate scenes
+		for scene in root.get_owned_scenes():
+			_ur.add_do_method(scene.get_parent(), &"add_child", scene)
+			_ur.add_do_property(scene, &"owner", scene.owner)
+			_ur.add_do_reference(scene)
+			_ur.add_undo_method(scene.get_parent(), &"remove_child", scene)
+		for info in cleared_scene_infos:
+			var node := info[&"node"] as Node
+			var parent := info[&"parent"] as Node
+			var owner := info[&"owner"] as Node
+			_ur.add_undo_method(parent, &"add_child", node)
+			_ur.add_undo_property(node, &"owner", owner)
+			_ur.add_undo_reference(node)
+	
+	_ur.commit_action(false)
 
 
 func snap_roots_areas(roots: Array[RoommateRoot]) -> void:
